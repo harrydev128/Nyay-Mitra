f04a8c26c75882e057da982c1e0fab7d2c614a44
dhirendram128 <dhirendram128@gmail.com>
2026-03-20 19:36:56 +0530

    Fix: production API URL set

--- frontend/constants/Config.ts
+++ frontend/constants/Config.ts
@@ -1,5 +1,7 @@
 export const Config = {
-    API_URL: 'http://192.168.1.4:8001',
+    API_URL: __DEV__ 
+        ? 'http://192.168.1.4:8001'
+        : 'https://nyay-mitra-production.up.railway.app',
     APP_NAME: 'NyayMitra',
     VERSION: '1.0.0',
 };
