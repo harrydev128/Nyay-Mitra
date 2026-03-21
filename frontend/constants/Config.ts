11c034dc2ae4c4c6c03d97d78f400c04ee6b2019
dhirendram128 <dhirendram128@gmail.com>
2026-03-22 01:29:29 +0530

    Fix: always use Railway API URL

--- frontend/constants/Config.ts
+++ frontend/constants/Config.ts
@@ -1,7 +1,5 @@
 export const Config = {
-    API_URL: __DEV__ 
-        ? 'http://192.168.1.4:8001'
-        : 'https://nyay-mitra-production.up.railway.app',
+    API_URL: 'https://nyay-mitra-production.up.railway.app',
     APP_NAME: 'NyayMitra',
     VERSION: '1.0.0',
 };
